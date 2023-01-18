3d3412c94f784bdac95049671ecfd08f2bfb8eb6
VIVID89 <wskth0930@gmail.com>
2023-01-18 21:17:23 +0900

    미션 2 2번째 시도

--- prac01.js
+++ prac01.js
@@ -1,3 +1,33 @@
-let name = "ilya";
+var sizes = [
+  [60, 50],
+  [40, 70],
+  [50, 30],
+  [80, 40],
+];
 
-alert( 'hello' ${name});
+function solution(sizes) {
+  var wmaxsize = 0;
+  var hmaxsize = 0;
+
+  for (let i = 0; i < sizes.length; i++) {
+    let a = sizes[i][0];
+    let b = sizes[i][1];
+
+    if (a > b) {
+      if (a > wmaxsize) {
+        wmaxsize = a;
+      }
+      if (b > hmaxsize) {
+        hmaxsize = b;
+      }
+    } else {
+      if (a > hmaxsize) {
+        hmaxsize = a;
+      }
+      if (b > wmaxsize) {
+        wmaxsize = b;
+      }
+    }
+  }
+  console.log(a, b);
+}
