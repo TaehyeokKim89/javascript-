25dbb1c9260cbf0b680cb4f8904b9e30d8034d26
VIVID89 <wskth0930@gmail.com>
2023-01-19 21:57:20 +0900

    코딩테스트 문자열 내 마음대로 정렬하기

--- object.js
+++ object.js
@@ -1,8 +1,18 @@
-let style = ["Jazz", "Blues"];
+let strings = ["sun", "bed", "car"];
 
-style.push("Rock-nRoll");
-style[1] = "Classics";
-style.shift();
-style.unshift("Rap", "Reggae");
+function solution(strings, n) {
+  let tempArray = [];
+  let answer = [];
+  for (let string of strings) {
+    tempArray.push(string[n] + string); // 1. 문자열 앞에 인덱스에 해당하는 문자를 붙인다.
+  }
+  tempArray.sort(); // 2. 사전순으로 정렬한다.
+  for (let a of tempArray) {
+    answer.push(a.substr(1)); // 3. 정렬된 배열의 가장 앞 글자를 땐다.
+  }
 
-console.log(style);
+  return answer;
+}
+
+let sol = solution(strings, 1);
+console.log(sol);
