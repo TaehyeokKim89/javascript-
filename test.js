368c044d627900445e5e132c3865598cd3ff7853
VIVID89 <wskth0930@gmail.com>
2023-01-19 10:27:22 +0900

    if 문 사용하지 않고 ?,|| 를 사용하여 본문 작성

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-function solution(num1, num2) {
-  return parseInt(num1 / num2);
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm("보호자의 동의를 받으셨나요?");
+  }
 }
+// if문을 사용하지 않고 동일한 동작을 하는 함수를 한 줄에 작성해보세요.
 
-let n1 = 3;
-let n2 = 2;
-console.log(solution(n1, n2));
+// 아래 조건을 충족하는 해답 2개를 작성해야 합니다.
+
+// 물음표 연산자 ?를 사용하여 본문을 작성
+// OR 연산자 ||를 사용하여 본문을 작성
+
+// 1.
+function checkAge(age) {
+  return age > 18 ? true : confirm("보호자의 동의를 받으셨나요?");
+}
+// 2.
+function checkage(age) {
+  return age > 18 || confirm("보호자의 동의를 받으셨나요?");
+}
