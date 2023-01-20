b078ebcea5a8334fee1f193b08be0a6b012d20c2
VIVID89 <wskth0930@gmail.com>
2023-01-20 22:53:35 +0900

    숫자 오름차순 정렬 sort의 비교함수 넣기

--- test05.js
+++ test05.js
@@ -1,1 +1,13 @@
+let sides = [199,72, 222];
+function solution(sides) {
+    let a = sides.sort((a, b) => (a - b));
+    if (a[0]+a[1] > a[2]) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+let b = solution(sides)
+console.log(b);
 
