e6fb6643d9224a108da786b1b26423e0880cf100
VIVID89 <wskth0930@gmail.com>
2023-01-20 22:31:01 +0900

    삭제~~!!

--- test05.js
+++ test05.js
@@ -1,14 +1,1 @@
-let my_string = 'hello';
-function solution(my_string, n) {
-    let answer = ""
-    for( let i = 0; i < my_string.length; i++){
-        let a = my_string.charAt(i).repeat(n);
-        answer += a;
-    } return answer;
-}
 
-
-
-let c = solution(my_string, 3)
-
-console.log(c);
