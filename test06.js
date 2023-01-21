7ccea093c96b09c253873cdb44771a435715fc0e
VIVID89 <wskth0930@gmail.com>
2023-01-21 20:54:01 +0900

    리셋55

--- test06.js
+++
@@ -1,9 +0,0 @@
-function solution(my_string) {
-    let answer = 0;
-    let str = my_string.replace(/[^0-9]/g, '');
-    for (let i = 0; i < str.length; i++) {
-        answer += parseInt(str[i]);
-    }
-    return answer;
-}
-console.log(solution("1a2b3c4d123"));
