a09230f759c6b84aeea3c80d56135f4be2f4a53e
VIVID89 <wskth0930@gmail.com>
2023-01-24 15:05:54 +0900

    리셋212

--- test08.js
+++
@@ -1,8 +0,0 @@
-function solution(numbers, direction) {
-    if ( direction == "right") {
-        numbers.unshift(numbers.pop(numbers));
-    } else if ( direction == "left") {
-        numbers.push(numbers.shift(numbers));
-    }
-    return numbers;
-}
