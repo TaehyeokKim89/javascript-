86ef5b5408f336d0c2c035394c4a458345219847
VIVID89 <wskth0930@gmail.com>
2023-01-24 14:48:09 +0900

    리셋 66

--- test08.js
+++
@@ -1,9 +0,0 @@
-function solution(array) {
-    let answer = [];
-    answer.push(Math.max(...array));
-    answer.push(array.indexOf(Math.max(...array)));
-    return answer;
-}
-
-console.log(solution(
-[1, 8, 3]))
