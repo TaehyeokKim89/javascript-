cdc2af655f73676e3c71a0f5c998ca36bed2e02f
VIVID89 <wskth0930@gmail.com>
2023-01-24 14:47:34 +0900

    배열에서 가장큰 수 와 인덱스값 찾기

---
+++ test08.js
@@ -0,0 +1,9 @@
+function solution(array) {
+    let answer = [];
+    answer.push(Math.max(...array));
+    answer.push(array.indexOf(Math.max(...array)));
+    return answer;
+}
+
+console.log(solution(
+[1, 8, 3]))
