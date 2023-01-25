0d6c28719a3fb23c2357c3f11c0168c0282c9f02
VIVID89 <wskth0930@gmail.com>
2023-01-25 22:49:17 +0900

    리셋11

--- test09.js
+++
@@ -1,7 +0,0 @@
-function solution(a, b, n) {
-    let coke = 0; // 돌려 받을 빈 콜라병
-    while (n >= a) { // n이 a보다 크거나 같다면
-        coke += Math.floor(n/a)*b // coke에 n/a의 몫의 정수값에 곱하기 b를 해준다.
-        n = Math.floor(n/a)*b + n%a;// n/a의 몫*b 에 n/a 나머지값을 더하면 남은 콜라가 된다. 이 콜라병으로 다시 반복한다. 
-    } return coke; 
-}
