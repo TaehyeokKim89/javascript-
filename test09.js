8ee3ddc8f967d724cb50aeaf88c3e0c8475b56c2
VIVID89 <wskth0930@gmail.com>
2023-01-25 10:56:25 +0900

    리셋01

--- test09.js
+++
@@ -1,8 +0,0 @@
-function solution(my_string) {
-    let a = my_string.split("");
-    let b = [];
-    for (let i = 0; i < a.length; i++) {
-        b.push(a[i].toLowerCase());
-    }
-    return b.sort().join("");
-}
