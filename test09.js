cc2539f0d6561d878fcc3d101399e05d9a1087da
VIVID89 <wskth0930@gmail.com>
2023-01-25 22:42:29 +0900

    콜라 문제 while 활용

--- test09.js
+++ test09.js
@@ -1,16 +1,7 @@
-let a = 3;
-let b = 1;
-let n = 20;
 function solution(a, b, n) {
-    let count = 0;
-    for (let i = 1; i < n; i++) {
-        if (i % a == 0) {
-            count += 1;
-
-        }
-    }
-    return count
+    let coke = 0; // 돌려 받을 빈 콜라병
+    while (n >= a) { // n이 a보다 크거나 같다면
+        coke += Math.floor(n/a)*b // coke에 n/a의 몫의 정수값에 곱하기 b를 해준다.
+        n = Math.floor(n/a)*b + n%a;// n/a의 몫*b 에 n/a 나머지값을 더하면 남은 콜라가 된다. 이 콜라병으로 다시 반복한다. 
+    } return coke; 
 }
-
-let aa = solution(a,b,n);
-console.log(aa);
