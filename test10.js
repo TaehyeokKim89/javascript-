ca05c89f2c385d71c3dcb167f413dd06bd76842d
VIVID89 <wskth0930@gmail.com>
2023-01-26 23:47:44 +0900

    리셋01

--- test10.js
+++
@@ -1,9 +0,0 @@
-function solution(num, k) {
-  let number = num.toString().split("").map(Number); //
-  let a = number.indexOf(k, 0);
-  if (a != -1) {
-    return a + 1;
-  } else {
-    return -1;
-  }
-}
