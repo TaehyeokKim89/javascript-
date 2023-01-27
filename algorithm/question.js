2d1ac9b88821b3834c7b40edcee127ba5ddff3e8
VIVID89 <wskth0930@gmail.com>
2023-01-27 13:18:01 +0900

    두 정수 사이의합 Math.max min 의 활용

--- algorithm/question.js
+++ algorithm/question.js
@@ -1,1 +1,10 @@
-
+function solution(a, b) {
+  let sum = 0;
+  let max = Math.max(a, b);
+  let min = Math.min(a, b);
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(solution(5, 3));
