cea3d791f4bd1eeac13de692016c7ab972e543d9
VIVID89 <wskth0930@gmail.com>
2023-01-27 19:28:55 +0900

    리셋10

--- algorithm/question.js
+++
@@ -1,7 +0,0 @@
-let a = 6;
-let b = 24;
-let week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];//요일명이 담긴 배열을 만든다.
-let date = new Date(`2016-${a}-${b}`).getDay(); //Date 함수는 날짜를 
-let dateNumber = week[date];
-
-console.log(dateNumber);
